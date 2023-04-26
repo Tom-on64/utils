c9ee4b4d30bc97484f2a35e81e412d3095090e3a
Tom-on64 <kubecekt@gmail.com>
2023-04-26 13:26:14 +0200

    Fixed RNG Added collision detection to RNG render

--- public/rng/app.js
+++ public/rng/app.js
@@ -11,6 +11,9 @@ let m = Math.pow(2, 32);
 
 const random = () => {
   seed = (seed * a + c) % m;
+
+  document.getElementById("seedDisplay").textContent = seed;
+
   return seed / m;
 };
 
@@ -28,16 +31,24 @@ const generateNumber = () => {
 };
 
 let y = 0;
+let collisionCheck = [];
 
 const render = () => {
   for (let x = 0; x < canvas.width; x++) {
     const r = random();
-
     ctx.fillStyle = r < 0.5 ? "white" : "black";
+
+    if (collisionCheck[r * m]) {
+      if (r < 0.5) ctx.fillStyle = "#FF6384";
+      else ctx.fillStyle = "#BB0011";
+    }
+    collisionCheck[r * m] = true;
+
     ctx.fillRect(x, y, 1, 1);
   }
   if (y < canvas.height) y++;
   else return;
+
   requestAnimationFrame(render);
 };
 
@@ -45,27 +56,25 @@ document.getElementById("generatorForm").onsubmit = (e) => {
   e.preventDefault();
 
   y = 0;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  seed = e.target[0].value;
-  a = e.target[1].value;
-  c = e.target[2].value;
-  m = e.target[3].value;
+  collisionCheck = [];
+  seed = parseInt(e.target[0].value);
+  a = parseInt(e.target[1].value);
+  c = parseInt(e.target[2].value);
+  m = parseInt(e.target[3].value);
 
   render();
-
-  seed = e.target[0].value;
 };
 
 const reset = () => {
-  const v = document.getElementById("generatorForm").children;
-  v[0].children[1].value = seed;
-  v[1].children[1].value = a;
-  v[2].children[1].value = c;
-  v[3].children[1].value = m;
+  document.getElementById("seed").value = 7363738;
+  document.getElementById("a").value = 1664525;
+  document.getElementById("c").value = 1013904223;
+  document.getElementById("m").value = Math.pow(2, 32);
 
   seed = 7363738;
   a = 1664525;
   c = 1013904223;
   m = Math.pow(2, 32);
+
+  document.getElementById("seedDisplay").textContent = seed;
 };
