f8b9ad09e343368f82f60afdcd03dbd85ac06867
Tom-on64 <kubecekt@gmail.com>
2023-05-03 21:42:17 +0200

    Added functionality to omega8 emulator TODO: fix SAB ins

---
+++ public/emu/omega8/app.js
@@ -0,0 +1,214 @@
+const input = document.getElementById("code");
+const emu = {
+  registers: {
+    A: "00000000",
+    B: "00000000",
+    F: "00000000",
+    I: "00000000",
+    O: "00000000",
+    S: "00000000",
+    P: "0000",
+  },
+  ram: [
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+  ],
+  compiled: [
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+    "00000000",
+  ],
+};
+const ins = {
+  HLT: "0000",
+  OUT: "0001",
+  LDA: "0010",
+  STA: "0011",
+  ADD: "0100",
+  SUB: "0101",
+  NOP: "0110",
+  LDB: "0111",
+  LSH: "1000",
+  RSH: "1001",
+  JMP: "1010",
+  JNZ: "1011",
+  INC: "1100",
+  DEC: "1101",
+  ATS: "1110",
+  SAB: "1111",
+  SET: "set",
+};
+
+const getRam = (add) => {
+  return emu.ram[parseInt(add, 2)];
+};
+
+const setRam = (add, val) => {
+  emu.ram[parseInt(add, 2)] = val;
+};
+
+const getReg = (reg) => {
+  return emu.registers[reg.toUpperCase()];
+};
+
+const setReg = (reg, val) => {
+  emu.registers[reg.toUpperCase()] = val;
+};
+
+const setFlag = (index, val) => {
+  const f = getReg("F");
+  nf = f.substring(0, index) + val + f.substring(index + 1);
+  setReg("F", nf);
+};
+
+const reset = () => {
+  const clearedRam = [];
+  for (let i = 0; i < 16; i++) clearedRam.push("00000000");
+  emu.ram = clearedRam;
+
+  emu.registers.A = "00000000";
+  emu.registers.B = "00000000";
+  emu.registers.F = "00000000";
+  emu.registers.I = "00000000";
+  emu.registers.O = "00000000";
+  emu.registers.P = "0000";
+};
+
+const step = () => {
+  if (getReg("F")[7] == "1") {
+    document.getElementById("warning").innerText =
+      "WARNING: Processor is halted!";
+    return;
+  } else document.getElementById("warning").innerText = "";
+
+  const fullI = getRam(emu.registers.P);
+  setReg("I", fullI);
+  const i = fullI.slice(0, 4);
+  const o = fullI.slice(4, 8);
+
+  console.log(i);
+
+  if (i == "0000") {
+    setFlag(7, "1");
+    return;
+  } else if (i == "0001") setReg("O", getRam(o));
+  else if (i == "0010") setReg("A", getRam(o));
+  else if (i == "0011") setRam(o, getReg("A"));
+  else if (i == "0100")
+    setReg(
+      "A",
+      (parseInt(getReg("A"), 2) + parseInt(getReg("B"), 2))
+        .toString(2)
+        .padStart(8, "0")
+    );
+  else if (i == "0101")
+    setReg(
+      "A",
+      (parseInt(getReg("A"), 2) - parseInt(getReg("B"), 2))
+        .toString(2)
+        .padStart(8, "0")
+    );
+  else if (i == "0110") {
+    // NOP
+  } else if (i == "0111") setReg("B", getRam(o));
+  else if (i == "1000")
+    setReg("A", (parseInt(getReg("A"), 2) << 1).toString(2).padStart(8, "0"));
+  else if (i == "1001")
+    setReg("A", (parseInt(getReg("A"), 2) >> 1).toString(2).padStart(8, "0"));
+  else if (i == "1010") {
+    setReg("P", o);
+    return;
+  } else if (i == "1011") {
+    if (getReg("F")[0] == 0) {
+      setReg("P", o);
+      return;
+    }
+  } else if (i == "1100")
+    setReg("A", (parseInt(getReg("A"), 2) + 1).toString(2).padStart(8, "0"));
+  else if (i == "1101")
+    setReg("A", (parseInt(getReg("A"), 2) - 1).toString(2).padStart(8, "0"));
+  else if (i == "1110") setReg("A", getReg("S"));
+  else if (i == "1111") {
+    console.log("swap");
+    const temp = getReg("A");
+    setReg("A", getReg("B"));
+    setReg("B", temp);
+  } else console.log("HELP");
+
+  setFlag(0, parseInt(getReg("A"), 2) == 0 ? "1" : "0");
+  setReg("P", (parseInt(getReg("P"), 2) + 1).toString(2).padStart(4, "0"));
+  if (parseInt(getReg("P"), 2) > 15) setReg("P", "0000");
+};
+
+const compile = () => {
+  const asm = input.value.split("\n");
+  let p = 0;
+
+  for (let i = 0; i < asm.length; i++) {
+    const l = asm[i].split(" ");
+    const o = ins[l[0].toUpperCase()];
+    if (!o) continue;
+
+    if (o === "set") {
+      if (!l[1] || !l[2]) continue;
+      emu.compiled[parseInt(l[1], 2)] = l[2];
+      continue;
+    }
+
+    emu.compiled[p] = `${o}${
+      l[1] ? parseInt(l[1], 2).toString(2).padStart(4, "0") : "0000"
+    }`;
+    p++;
+  }
+};
+
+const reload = () => {
+  reset();
+  emu.ram = emu.compiled;
+};
+
+const render = () => {
+  requestAnimationFrame(render);
+
+  const ram = document.querySelector("#ram table").children[0].children;
+  for (let i = 0; i < emu.ram.length; i++) {
+    a = emu.ram[i];
+    ram[i + 1].children[1].innerText = a;
+  }
+
+  const reg = document.querySelector("#registers table").children[0].children;
+  const regK = Object.keys(emu.registers);
+  for (let i = 0; i < regK.length; i++) {
+    a = emu.registers[regK[i]];
+    reg[i + 1].children[1].innerText = a;
+  }
+};
+
+render();
--- public/emu/omega8/design.md
+++ public/emu/omega8/design.md
@@ -7,8 +7,8 @@ Max 16Hz clock
 
 ## Instructions
  BIN INS      | Description                                      | Operation
-0000 NUL      | NULL                                             | 0 
-0001 OUT      | Outputs A                                        | O = A 
+0000 HLT      | Halts the processor                              | {Stops Processor} 
+0001 OUT<add> | Outputs value at address                         | O = [add] 
 0010 LDA<add> | Sets A to value from address                     | A = [add] 
 0011 STA<add> | Sets address to value from A                     | [add] = A 
 0100 ADD      | Sets A to the sum of A and B                     | A += B 
@@ -21,8 +21,10 @@ Max 16Hz clock
 1011 JNZ<add> | Jumps to the address if the IsZero flag is zero  | if (F[0] == 0) P = add 
 1100 INC      | Increments A by one                              | A++ 
 1101 DEC      | Decrements A by one                              | A-- 
-1110 
-1111 HLT      | Halts the processor                              | {Stops Processor} 
+1110 ATS      | Sets A to value at S                             | A = S
+1111 SAB      | Swaps values of A and B                          | {A swaps with B}
+### Special
+SET<add><val> | Sets value at address                            | [add] = val
 
 ## Registers
 A - 8BIT General purpose register
@@ -30,7 +32,18 @@ B - 8BIT General purpose register
 F - 8BIT Flag register
 I - 8BIT Instruction register
 O - 8BIT Output register
+S - 8BIT Serial Input register
 P - 4BIT Program counter
+### Flags
+_All get output on the controll pins
+F[0] - IsZero
+F[1] - 
+F[2] - 
+F[3] - 
+F[4] - 
+F[5] - 
+F[6] - 
+F[7] - IsHalted
 
 ## Pins (32)
 ### IN (16)
@@ -43,6 +56,34 @@ RESET - Reset pin
 
 ### OUT (14)
 O0-7  - Output pins
-C0-7  - Control pins
+C0-7  - Controll pins
 
 ### 3V, GND
+
+## Example code
+### Code
+SET 1111 01110111
+SET 1110 01110110
+LDA 1111
+LDB 1110
+SUB
+STA 1101
+OUT 1101
+HLT
+### Expected Binary
+00101111
+01111110
+01010000
+00111101
+00011101
+11110000
+00000000
+00000000
+00000000
+00000000
+00000000
+00000000
+00000000
+00000000
+01110110
+01110111
